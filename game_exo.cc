c1eb9984906efb835807dca3bc1a0a441022d587
Olivier Ly <ly.olivier@gmail.com>
2014-10-16 09:41:57 +0700

    nouveau fichier

---
+++ game_exo.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main() {
+
+  int n=0;
+  while (1) {
+    system("clear"); /* efface l'écran du terminal */
+    for (int i=0; i<n; i++) 
+      cout << endl;
+    cout << "hello world!" << endl;
+    
+    n = (n+1) % 20;
+    usleep(20 * 1000);
+  }
+}
